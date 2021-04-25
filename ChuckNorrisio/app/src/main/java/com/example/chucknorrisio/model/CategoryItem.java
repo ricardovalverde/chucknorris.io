85c0d20c7a9cf7a5af2713876e9e11e9e5d7aa63
ricardovalverde <81026247+ricardovalverde@users.noreply.github.com>
2021-04-25 00:39:58 -0300

    update 24-ABR

---
+++ ChuckNorrisio/app/src/main/java/com/example/chucknorrisio/model/CategoryItem.java
@@ -0,0 +1,36 @@
+package com.example.chucknorrisio.model;
+
+import android.support.annotation.NonNull;
+import android.widget.TextView;
+
+import com.example.chucknorrisio.R;
+import com.xwray.groupie.Item;
+import com.xwray.groupie.ViewHolder;
+
+public class CategoryItem extends Item<ViewHolder> {
+
+    private final String categoryName;
+    private final int bgColor;
+
+    public CategoryItem (String categoryName, int bgColor){
+        this.categoryName = categoryName;
+        this.bgColor = bgColor;
+    }
+
+
+
+
+
+    @Override
+    public void bind(@NonNull ViewHolder viewHolder, int position) {
+        TextView txtCategory = viewHolder.itemView.findViewById(R.id.txt_category);
+        txtCategory.setText(categoryName);
+        viewHolder.itemView.setBackgroundColor(bgColor);
+
+    }
+
+    @Override
+    public int getLayout() {
+        return R.layout.card_category;
+    }
+}
